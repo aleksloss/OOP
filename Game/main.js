a8349531ab7ae5f47370f7177a764b7d52b0388c
Aleksandr Lõssikov <aleksandr.lossikov@ivkhk.ee>
2025-10-24 17:19:11 +0300

    rock-paper-scissors

--- Game/main.js
+++ Game/main.js
@@ -1,37 +1,46 @@
-//Импортируем необходимые модули
-//import modules
+// импортируем необходимые модули
+// import modules
 
 const readLineSync = require('readline-sync');
 
-//Имортрируем игровые модули
+//Импортируем игровые модули
 const runGuessTheNumber = require('./guess-the-number/game');
+const runTicTacToe = require('./tic-tac-toe/game');
+const runRockPaperScissors = require('./rock-paper-scissors/game');
 
-
-//Контроллер игрового цикла
+//контроллер игрового цикла
 //Game loop controller
 let keepPlaying = true;
 
 //Основной цикл меню. Будет продолжаться, пока игрок не захочет выйти
 //Main menu loop. Will continue until the player wants to exit
+
 while (keepPlaying) {
     console.clear();
-    console.log("================================");
-    console.log("=      WELCOME TO THE GAME     =");
-    console.log("================================");
+    console.log("====================");
+    console.log("=welcome to the game=");
+    console.log("====================");
     console.log("Which game mode do you want to play?");
 
     const games = [
-        'Guess the Number'
+        'Guess the Number',
+        'Tic Tac Toe',
+        'Rock Paper Scissors'
     ];
-    
     //отображаем доступные игры
-    const index = readLineSync.keyInSelect(games, 'Choose a game or exit:');
+    const index = readLineSync.keyInSelect(games, 'Choose a game or exit');
 
-    //в зависимости от выбора игрока запускаем соответствующую игру
+    //в зависимости от выбора игрока запускаем соответсвующую игру
     switch (index) {
         case 0:
             runGuessTheNumber();
             break;
+        case 1:
+            runTicTacToe();
+            break; 
+        case 2:
+            runRockPaperScissors();
+            break;    
         //дальше будет добавляться новые игры
         default:
             console.log("Exiting the game. Goodbye!");
@@ -40,7 +49,7 @@ while (keepPlaying) {
     }
     //После завершения игры спрашиваем, хочет ли игрок сыграть снова
     if (keepPlaying) {
-        if (!readLineSync.keyInYN('Do you want to another game?')) {
+        if (!readLineSync.keyInYN('Do you want to another game?')){
             keepPlaying = false;
             console.log("Goodbye!");
         }
